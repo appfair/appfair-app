db71ec96c75c80e7c3d948836abf4e5b38f9071f
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-04-04 14:14:11 -0400

    Update dependencies and licenses

--- Package.swift
+++ Package.swift
@@ -1,25 +1,20 @@
 // swift-tools-version: 5.9
-// This is a Skip (https://skip.dev) package,
-// containing a Swift Package Manager project
-// that will use the Skip build plugin to transpile the
-// Swift Package, Sources, and Tests into an
-// Android Gradle Project with Kotlin sources and JUnit tests.
 import PackageDescription
 
 let package = Package(
     name: "appfair-app",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
         .library(name: "AppFairUI", type: .dynamic, targets: ["AppFairUI"]),
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.2.23"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0"),
+        .package(url: "https://source.skip.tools/skip-kit.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "AppFairUI", dependencies: [
-            .product(name: "SkipUI", package: "skip-ui"),
+            .product(name: "SkipKit", package: "skip-kit"),
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "AppFairUITests", dependencies: [
             "AppFairUI",
